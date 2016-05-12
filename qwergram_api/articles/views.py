cdf667347fae164c62b49df3c3640ee48557736a
Norton Pengra <npengra317@gmail.com>
2016-05-11 19:04:04 -0700

    New viewsets for User/Group model (#1)

--- qwergram_api/articles/views.py
+++ qwergram_api/articles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from articles.serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User, Group
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """API endpoint that edits/views User models."""
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """API endpoint that edits/views Group models."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
