1a6c7edd6da98abb1ad15c3a9d8b29d463304490
Norton Pengra <npengra317@gmail.com>
2016-05-11 19:39:39 -0700

    Viewset created for CodeArticle (#3)

--- qwergram_api/articles/views.py
+++ qwergram_api/articles/views.py
@@ -1,15 +1,24 @@
 from rest_framework import viewsets
-from articles.serializers import UserSerializer, GroupSerializer
 from django.contrib.auth.models import User, Group
+from articles import models
+from articles import serializers
 
 # Create your views here.
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """API endpoint that edits/views User models."""
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
+
 
 class GroupViewSet(viewsets.ModelViewSet):
     """API endpoint that edits/views Group models."""
     queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    serializer_class = serializers.GroupSerializer
+
+
+class CodeArticleViewSet(viewsets.ModelViewSet):
+    """API endpoint that edits/views CodeArticle models."""
+    queryset = models.CodeArticleModel.objects.all()
+    serializer_class = serializers.CodeArticleSerializer
