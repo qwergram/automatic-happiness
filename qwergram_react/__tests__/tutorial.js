0b1b604432a0938a86567fb2c8e340e6e1324e80
Norton Pengra <npengra317@gmail.com>
2016-05-12 12:20:19 -0700

    Configured Jest (#25)

---
+++ qwergram_react/__tests__/tutorial.js
@@ -0,0 +1,29 @@
+jest.unmock('../tutorial1');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import CommentBox from '../tutorial1';
+
+describe('CommentBox', () => {
+
+  it('contains the right text', () => {
+    // Render a checkbox with label in the document
+    const checkbox = TestUtils.renderIntoDocument(
+      <CommentBox />
+    );
+
+    const checkboxNode = ReactDOM.findDOMNode(checkbox);
+
+    // Verify that it's Off by default
+    // expect(checkboxNode.textContent).toEqual('Off');
+
+    // Simulate a click and verify that it is now On
+    // TestUtils.Simulate.change(
+      // TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
+    // );
+
+    expect(checkboxNode.children).toEqual('Hello World! I am a comment box!');
+  });
+
+});
