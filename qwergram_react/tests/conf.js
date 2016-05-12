6c3c077e6909cbc1ea311c3dff901812f6a1fe1f
Norton Pengra <npengra317@gmail.com>
2016-05-12 12:11:46 -0700

    Switching to Jest (#25)

--- qwergram_react/tests/conf.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  specs: ['tutorial.js'],
-  capabilities: {
-    browserName: 'chrome'
-  },
-  baseUrl: 'http://127.0.0.1:3000',
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-  framework: 'jasmine',
-}
