55043133e7bf4ce3a0503b32338631c6780edca8
Norton Pengra <npengra317@gmail.com>
2016-05-11 20:58:01 -0700

    permission_test for login (#2)

---
+++ qwergram_api/articles/tests/test_permissions.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, Client
+from articles import views
+
+
+class CreateUsers(TestCase):
+
+    def setUp(self):
+
+        from django.contrib.auth.models import User
+        titan = User(
+            username='titan',
+            email='titan@destiny.com',
+            first_name='titan',
+            last_name='lastname'
+        )
+        titan.set_password('password1')
+
+        self.user_client = Client()
+        self.client = Client()
+        self.login_okay = self.user_client.post(
+            '/api/v1/api-auth/login/',
+            {
+                'username': 'titan',
+                'password': 'password1',
+
+            }
+        )
+
+    def test_login_okay(self):
+        self.assertEquals(self.login_okay.status_code, 200)
