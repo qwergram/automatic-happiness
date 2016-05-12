c3cade165f31ecabb04985c006401a7b1efd60cc
Norton Pengra <npengra317@gmail.com>
2016-05-11 19:40:01 -0700

    Updated routes (#3)

--- qwergram_api/articles/urls.py
+++ qwergram_api/articles/urls.py
@@ -5,6 +5,7 @@ from articles import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'articles', views.CodeArticleViewSet)
 
 urlpatterns = [
     url(r'api-auth/', include('rest_framework.urls')),
