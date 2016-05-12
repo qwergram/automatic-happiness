33d6f97746683f9664289297b6fd31fa6f607430
Norton Pengra <npengra317@gmail.com>
2016-05-11 19:08:35 -0700

    Added routes for articles api (#1)

---
+++ qwergram_api/articles/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from articles import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    url(r'api-auth/$', include('rest_framework.urls')),
+    url(r'/$', include(router.urls)),
+]
