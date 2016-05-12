4d949fba2ef4e740d5b10f7b612a676fa562480d
Norton Pengra <npengra317@gmail.com>
2016-05-12 11:05:13 -0700

    Renamed conf and spec files (#25)

--- qwergram_react/tests/spec.js
+++ qwergram_react/tests/spec.js
@@ -4,4 +4,9 @@ describe('test comments show up', function() {
     expect(1).toEqual(1);
   });
 
+  it('has the right title', function() {
+    browser.get('test');
+    bad_url = browser.getCurrentUrl();
+  });
+
 });
