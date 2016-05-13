32f770cdd802c002dc99d5faf7855c7af80cc6c5
Norton Pengra <npengra317@gmail.com>
2016-05-12 20:22:35 -0700

    Created static_root (#28)

--- qwergram_api/qwergram_api/settings.py
+++ qwergram_api/qwergram_api/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '/var/www/static/'
 
 # Django Rest-Framework
 # https://www.django-rest-framework.org/
