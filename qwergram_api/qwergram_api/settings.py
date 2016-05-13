c0cc010bb248e7a050cdf210ea0ef6d86b5a4875
Norton Pengra <npengra317@gmail.com>
2016-05-12 20:37:42 -0700

    Fixed debug param (#28)

--- qwergram_api/qwergram_api/settings.py
+++ qwergram_api/qwergram_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'not-so-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG_MODE'] == 'True'
 
 ALLOWED_HOSTS = [
     'localhost',
