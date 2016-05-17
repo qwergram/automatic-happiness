bd12919f7c13a03815cf579ac157ec072d474af9
Norton Pengra <npengra317@gmail.com>
2016-05-17 10:30:11 -0700

    Mocked OfflineHydrogen params (#5)

--- qwergram_bots/smtp/tests/conftest.py
+++ qwergram_bots/smtp/tests/conftest.py
@@ -4,6 +4,10 @@ from smtp.get_articles import Hydrogen
 
 class OfflineHydrogen(Hydrogen):
 
+    email_addr = "test@test.com"
+    email_pass = "amazing_password1"
+    email_imap = "imap.totally_valid_server.net"
+
     def connect(self):
         self.connected = True
         self.mail = object
