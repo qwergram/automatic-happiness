7ca759ceaeee97c5f18103107dd1cd6985205cf3
Norton Pengra <npengra317@gmail.com>
2016-05-16 19:50:06 -0700

    Created submit_idea funcitonality for smtp bot (#5)

--- qwergram_bots/smtp/read_updates_from_email.py
+++ qwergram_bots/smtp/read_updates_from_email.py
@@ -6,15 +6,20 @@ import imaplib
 import email
 import datetime
 import json
+import requests
 
 
 EMAIL_ADDR = os.environ['EMAIL_ADDR']
 EMAIL_PASS = os.environ['EMAIL_PASS']
 EMAIL_IMAP = os.environ['EMAIL_IMAP']
 EMAIL_ADMIN = os.environ['EMAIL_ADMIN']
-
+LOCAL_ENDPOINT = "http://127.0.0.1:8000/api/v1/"
 
 class EmailClient(object):
+
+    idea_endpoint = LOCAL_ENDPOINT + "ideas/"
+    share_endpoint = LOCAL_ENDPOINT + "shares/"
+    article_endpoint = LOCAL_ENDPOINT + "articles/"
 
     def __init__(self):
         self.email_addr = EMAIL_ADDR
@@ -46,6 +51,23 @@ class EmailClient(object):
                 if json_data:
                     self.save_json_data(json_data)
             self.mail.copy(email_num, b'[Gmail]/Trash')
+
+    def save_json_data(self, json_data):
+        submission_type = json_data['type']
+        return getattr("submit_" + submission_type)(json_data)
+
+    def submit_idea(self, json_data):
+        return requests.post(
+            self.idea_endpoint,
+            data={
+                "title": json_data['title'],
+                "pitch": json_data['pitch'],
+                "priority": json_data['priority'],
+            }
+        )
+
+    def submit_share(self, json_data):
+        pass
 
     def get_json_from_payload(self, message):
         payload = str(message.get_payload()[0])
@@ -85,12 +107,16 @@ class EmailClient(object):
         from_addr = message['from']
         time_sent = message['Date']
         is_admin = EMAIL_ADMIN in from_addr
-        if is_admin:
-            json_data = getattr(self, "validify_" + submission_type.lower())(data_type, message)
-            if json_data:
-                json_data['time'] = time_sent
-                print(json.dumps(json_data, indent=2))
-                return json_data
+        try:
+            if is_admin:
+                json_data = getattr(self, "validify_" + submission_type.lower())(data_type, message)
+                if json_data:
+                    json_data['time'] = time_sent
+                    json_data['type'] = submission_type.lower()
+                    print(json.dumps(json_data, indent=2))
+                    return json_data
+        except AttributeError:
+            pass
         return False
 
     def is_valid_email(self, subject):
