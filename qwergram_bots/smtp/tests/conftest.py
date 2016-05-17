60cb3069181a42e4a24a726e71c4d867fd0135af
Norton Pengra <npengra317@gmail.com>
2016-05-17 10:58:34 -0700

    Specified email params for MockBot (#5)

--- qwergram_bots/smtp/tests/conftest.py
+++ qwergram_bots/smtp/tests/conftest.py
@@ -15,9 +15,6 @@ class MockMail(object):
 
 class OfflineHydrogen(Hydrogen):
 
-    email_addr = "test@test.com"
-    email_pass = "amazing_password1"
-    email_imap = "imap.totally_valid_server.net"
 
     def connect(self):
         self.connected = True
@@ -25,4 +22,8 @@ class OfflineHydrogen(Hydrogen):
 
 @pytest.fixture
 def HydrogenBot():
-    return OfflineHydrogen()
+    return OfflineHydrogen(
+        email_addr="test@test.com",
+        email_pass="amazing_password1",
+        email_imap="imap.totally_valid_server.net",
+    )
