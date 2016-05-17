8491dd047b8094997b06239ea32ee701059604d3
Norton Pengra <npengra317@gmail.com>
2016-05-16 21:44:11 -0700

    Moved variables to __init__ for future QPM (#5)

--- qwergram_bots/smtp/__init__.py
+++ qwergram_bots/smtp/__init__.py
@@ -1,6 +1,25 @@
-__docs__ = """
+import os
+
+
+
+
+# Bot information for Qwergram Package Manager (QPM)
+__APIVERSION__ = "v1"
+__BOTNAME__ = "map_bot"
+__BOTVERSION__ = "v2"
+__DEPENDENCIES__ = []
+__DOCS__ = """
 
 This is a package for reading emails that I send to a specified email address
 and then publishing them to the aws instance.
 
 """
+
+# Globals that need to be defined
+EMAIL_ADDR = os.environ['EMAIL_ADDR']
+EMAIL_PASS = os.environ['EMAIL_PASS']
+EMAIL_IMAP = os.environ['EMAIL_IMAP']
+EMAIL_ADMIN = os.environ['EMAIL_ADMIN']
+ADMIN_USER = os.environ['ADMIN_USER']
+ADMIN_PASS = os.environ['ADMIN_PASS']
+LOCAL_ENDPOINT = "http://127.0.0.1:8000/api/{api_ver}/".format(__APIVERSION__)
