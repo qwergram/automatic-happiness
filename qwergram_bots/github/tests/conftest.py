6be319cbda26ef4a219ed8a9798ed630deeccf20
Norton Pengra <npengra317@gmail.com>
2016-05-20 18:01:16 -0700

    Updated OfflineHelium dependency injection (#65)

--- qwergram_bots/github/tests/conftest.py
+++ qwergram_bots/github/tests/conftest.py
@@ -9,7 +9,22 @@ class MockRequests(object):
 class OfflineHelium(Helium):
 
     def _hit_endpoint(self, target, verb="get"):
-        return MockRequests
+        return [{
+            "open_issues": 28,
+            "id": 58597301,
+            "html_url": "https://github.com/qwergram/automatic-happiness",
+            "clone_url": "https://github.com/qwergram/automatic-happiness.git",
+            "size": 928,
+            "full_name": "qwergram/automatic-happiness",
+            "homepage": "http://qwergram.github.io",
+            "pushed_at": "2016-05-20T22:13:08Z",
+            "updated_at": "2016-05-15T06:38:49Z",
+            "description": "Making the Martians jealous since 1942",
+            "watchers": 0,
+            "created_at": "2016-05-12T01:39:52Z",
+            "commits_url": "https://api.github.com/repos/qwergram/automatic-happiness/commits{/sha}",
+            "language": "Definitely not Assembly"
+        }]
 
 
 @pytest.fixture
