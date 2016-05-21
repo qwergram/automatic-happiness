78c974bb8bb0d1e09fc548ac6f640d8fa0fe27ed
Norton Pengra <npengra317@gmail.com>
2016-05-20 23:49:37 -0700

    Updated deployment script to github Actually includes valid commit messages

--- qwergram_devops/deploy_to_github.sh
+++ qwergram_devops/deploy_to_github.sh
@@ -1,13 +1,13 @@
 source ./../.secrets.sh
 mkdir ~/tmp/
 export original=$PWD/../qwergram_react
+export last_commit="`git log -1 --pretty=%B`"
 cd ~/tmp/
 git clone $CLIENT_REPO coding
 cd coding
 git pull origin master
 cp -a $original/. ~/tmp/coding/
 git add .
-git commit -m "Deployed automatically with qwergram.sh"
+git commit -m "$last_commit"
 git push origin master
-git status
 rm -rf ~/tmp/
