55c06ce4ff6d07acba54b39fe6d89d9bdcb3ccb8
Norton Pengra <npengra317@gmail.com>
2016-05-20 18:39:30 -0700

    Updated github deployment code (#73)

--- qwergram_devops/deploy_to_github.sh
+++ qwergram_devops/deploy_to_github.sh
@@ -6,7 +6,6 @@ git clone $CLIENT_REPO coding
 cd coding
 git pull origin master
 cp -a $original/. ~/tmp/coding/
-ls -a ~/tmp/coding/
 git add .
 git commit -m "Deployed automatically with qwergram.sh"
 git status
