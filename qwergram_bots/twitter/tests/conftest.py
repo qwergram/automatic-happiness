2be8f89004f6e0236ff29bcf5e7208e6033b3966
Norton Pengra <npengra317@gmail.com>
2016-05-22 15:58:13 -0700

    Created Requests and OfflineBeryllium mocks (#27)

---
+++ qwergram_bots/twitter/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from twitter.beryllium_bot import Beryllium
+
+
+class MockRequests(object):
+
+    class MockResponse(object):
+
+        ok = True
+
+        def json(self):
+            return {"some": "json", "json": True}
+
+    def get(self, *args, **kwargs):
+        return MockRequests.MockResponse
+
+    def post(self, *args, **kwargs):
+        return MockRequests.MockResponse
+
+
+class OfflineBeryllium(Beryllium):
+
+    @property
+    def get(self, *args, **kwargs):
+        return MockRequests().get
+
+    @property
+    def post(self, *args, **kwargs):
+        return MockRequests().post
+
+
+@pytest.fixture
+def BerylliumBot():
+    return OfflineBeryllium("consumer_key", "consumer_secret", "access_token", "access_secret")
