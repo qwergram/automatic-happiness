7997d243e8078e9030d30b38e4084d96906732bc
Norton Pengra <npengra317@gmail.com>
2016-05-22 16:28:16 -0700

    added super() statement (#27)

--- qwergram_bots/twitter/tests/conftest.py
+++ qwergram_bots/twitter/tests/conftest.py
@@ -4,21 +4,15 @@ from twitter.beryllium_bot import Beryllium, InvalidCredentials
 
 class MockRequests(object):
 
-    def __init__(self):
-        self.post_called = 0
-        self.get_called = 0
-
     class MockResponse(object):
 
         def json(self):
             return {"some": "json", "json": True}
 
     def get(self, *args, **kwargs):
-        self.get_called += 1
         return MockRequests.MockResponse()
 
     def post(self, *args, **kwargs):
-        self.post_called += 1
         return MockRequests.MockResponse()
 
 
@@ -26,6 +20,7 @@ class OfflineBeryllium(Beryllium):
 
     def __init__(self, *args, **kwargs):
         self.__TEST_ONLY_mock_requests = MockRequests()
+        super(OfflineBeryllium, self).__init__(*args, **kwargs)
 
     @property
     def get(self, *args, **kwargs):
