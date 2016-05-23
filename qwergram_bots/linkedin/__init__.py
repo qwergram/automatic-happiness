b052d20a5e600fb2ef07caa4b93ce5fe41d28e2f
Norton Pengra <npengra317@gmail.com>
2016-05-22 17:18:57 -0700

    Updated __DOCS__ on BoronBot

--- qwergram_bots/linkedin/__init__.py
+++ qwergram_bots/linkedin/__init__.py
@@ -4,5 +4,6 @@ __BOTNAME__ = ["Boron"]
 __BOTVERSION__ = "v0.1"
 __DEPENDENCIES__ = []
 __DOCS__ = """
-This bot will post to twitter whenever I post something cool.
+This bot will post to linkedin whenever I post something cool.
+In addition, synchronize my Resume to Linkedin.
 """
