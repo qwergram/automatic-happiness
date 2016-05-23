ba04111e1b692f30de7b6e8d0497154c6c9e4d16
Norton Pengra <npengra317@gmail.com>
2016-05-22 17:08:43 -0700

    Created QPM for BoronBot (#57)

---
+++ qwergram_bots/linkedin/__init__.py
@@ -0,0 +1,8 @@
+# Bot information for Qwergram Package Manager (QPM)
+__APIVERSION__ = "v1"
+__BOTNAME__ = ["Boron"]
+__BOTVERSION__ = "v0.1"
+__DEPENDENCIES__ = []
+__DOCS__ = """
+This bot will post to twitter whenever I post something cool.
+"""
