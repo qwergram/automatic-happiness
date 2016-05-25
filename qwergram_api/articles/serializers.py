e233f6d264a63fe5ef152be1140c727ab26388c7
Norton Pengra <npengra317@gmail.com>
2016-05-25 11:48:25 -0700

    Customize JSONField to work properly (#89)

--- qwergram_api/articles/serializers.py
+++ qwergram_api/articles/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from articles import models
+from articles.fields import JSONField
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -42,6 +43,6 @@ class StatSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer for the Stats model."""
     class Meta:
         model = models.StatModel
-        exclude = []
+        exclude = ['value']
 
-    value = serializers.JSONField()
+    value = JSONField()
