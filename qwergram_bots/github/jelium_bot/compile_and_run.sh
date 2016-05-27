feaae72486f265f3e6886018074d2444a7cb2c1c
Norton Pengra <npengra317@gmail.com>
2016-05-27 16:28:31 -0700

    wrapped existing code in function (#22)

--- qwergram_bots/github/jelium_bot/compile_and_run.sh
+++ qwergram_bots/github/jelium_bot/compile_and_run.sh
@@ -1,15 +1,25 @@
 # This script reads JeliumBot.java and reads all the class names and executes them
 # via "java $classname"
-file_name="JeliumBot.java"
-javac $file_name
-if [ $? == 0 ]; then
-  classes="`cat $file_name | grep class`"
-  last=""
-  for word in $(echo $classes | tr "\n" ";")
-  do
-    if [ "$last" == "class" ]; then
-      java $word
-    fi
-    last=$word
-  done
-fi
+
+clean() {
+  current_dir="`ls | grep .class`"
+  echo $current_dir
+}
+
+compile_and_execute() {
+  file_name="JeliumBot.java"
+  javac $file_name
+  if [ $? == 0 ]; then
+    classes="`cat $file_name | grep class`"
+    last=""
+    for word in $(echo $classes | tr "\n" ";")
+    do
+      if [ "$last" == "class" ]; then
+        java $word
+      fi
+      last=$word
+    done
+  fi
+}
+
+clean()
