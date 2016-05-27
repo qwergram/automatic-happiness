43909f1873d64ac50cfa3c2508649aa745261717
Norton Pengra <npengra317@gmail.com>
2016-05-27 12:58:13 -0700

    Renamed function (#22)

--- qwergram_bots/github/jelium_bot/jelium_bot.java
+++ qwergram_bots/github/jelium_bot/jelium_bot.java
@@ -2,7 +2,7 @@
  * This as a hello world app learned from https://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
 **/
 
-class jelium_bot {
+class HelloWorldApp {
   public static void main(String[] args) {
     System.out.println("Hello World!"); // Print out a string
   }
