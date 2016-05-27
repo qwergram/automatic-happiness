65bc44a3136861d99404a754cd97c378b2402b5a
Norton Pengra <npengra317@gmail.com>
2016-05-27 13:46:13 -0700

    Created http request reader (#22)

--- qwergram_bots/github/jelium_bot/JeliumBot.java
+++ qwergram_bots/github/jelium_bot/JeliumBot.java
@@ -1,15 +1,24 @@
 /**
  * This as a hello world app learned from https://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
+ * with help from http://interactivepython.org/courselib/static/java4python/Java4Python.html
+ * and http://stackoverflow.com/questions/1359689/how-to-send-http-request-in-java
 **/
 
-import java.net.HttpUrlConnection;
+import java.net.*;
+import java.io.*;
 
 public class JeliumBot {
-
-  enum Mode{ WAIT, SCRAPE, REPORT }
-  Mode mode;
-
-  public static void main(String[] args) {
-    System.out.println("Hello World!"); // Print out a string
+  public static void main(String[] args) throws Exception {
+    URL github = new URL("http://www.github.com");
+    URLConnection github_connection = github.openConnection();
+    BufferedReader github_in = new BuffereredReader(
+      new InputStreamReader(
+        github_connection.getInputStream()
+      )
+    );
+    String inputLine;
+    while ((inputLine = github_in.readLine()) != null)
+      System.out.println(inputLine);
+    github_in.close();
   }
 }
