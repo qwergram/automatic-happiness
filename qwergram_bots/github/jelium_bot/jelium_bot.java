39bcbae92bee38f39fa9bb634437e0932b08ee00
Norton Pengra <npengra317@gmail.com>
2016-05-27 13:04:53 -0700

    Added Mode to JeliumBot (#22)

--- qwergram_bots/github/jelium_bot/jelium_bot.java
+++ qwergram_bots/github/jelium_bot/jelium_bot.java
@@ -2,14 +2,12 @@
  * This as a hello world app learned from https://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
 **/
 
-class HelloWorldApp {
+class JeliumBot {
+
+  enum Mode{ WAIT, SCRAPE, REPORT }
+  Mode size;
+
   public static void main(String[] args) {
     System.out.println("Hello World!"); // Print out a string
-  }
-}
-
-class SecondWorldApp {
-  public static void main(String[] args) {
-    System.out.println("Hello Mars!");
   }
 }
