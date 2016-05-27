e149229c228ef0f3849a5e74299e4b5ef6a1544f
Norton Pengra <npengra317@gmail.com>
2016-05-27 13:39:25 -0700

    imported HttpUrlConnection (#22)

--- qwergram_bots/github/jelium_bot/jelium_bot.java
+++ qwergram_bots/github/jelium_bot/jelium_bot.java
@@ -2,7 +2,9 @@
  * This as a hello world app learned from https://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
 **/
 
-class JeliumBot {
+import java.net.HttpUrlConnection;
+
+public class JeliumBot {
 
   enum Mode{ WAIT, SCRAPE, REPORT }
   Mode mode;
@@ -10,16 +12,4 @@ class JeliumBot {
   public static void main(String[] args) {
     System.out.println("Hello World!"); // Print out a string
   }
-}
-
-class FreshJuiceTest {
-
-  public static void main(String[] args) {
-    JeliumBot jbot = new JeliumBot();
-    jbot.mode = JeliumBot.Mode.WAIT;
-    jbot.main(new String[0]);
-    // Interesting, This is very different than python
-    System.out.println("Mode: " + jbot.mode);
-  }
-
 }
