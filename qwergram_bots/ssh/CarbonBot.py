d77d09e0c0a1c0107cfbfac53f19fc78d0354121
Norton Pengra <npengra317@gmail.com>
2016-05-31 16:32:39 -0700

    Created get_git_ignore and ls functions (#99)

--- qwergram_bots/ssh/CarbonBot.py
+++ qwergram_bots/ssh/CarbonBot.py
@@ -1,8 +1,25 @@
 import os
+import io
 import sys
 
+
+def get_git_ignore(target):
+    try:
+        with io.open(os.path.join(target, '.gitignore')) as f:
+            gitignore = [ignore_this.strip() for ignore_this in f.readlines()]
+        return gitignore
+    except IOError:
+        return []
+
+
+def ls(target):
+    return os.listdir(target)
+
+
 def main(target):
-    os.system("ls " + target)
+    print(ls(target))
+    gitignore = get_git_ignore(target)
+    print(gitignore)
 
 
 if __name__ == "__main__":
