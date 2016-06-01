f4a071164d0c98f0df157ff67fba00e0b89f60b5
Norton Pengra <npengra317@gmail.com>
2016-05-31 17:22:51 -0700

    Basic test for oxygenbot (#22)

---
+++ qwergram_bots/github/tests/test_oxygen.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_oxygen_builds_correctly(OxygenBot):
+    assert OxygenBot.base_endpoint
+    assert OxygenBot.issues_target
+    assert OxygenBot.language_target
+    assert OxygenBot.commits_target
+    assert OxygenBot.milestone_target
+
+
+def test_online_bot(OnlineOxygenBot):
+    with pytest.raises(AttributeError):
+        OnlineOxygenBot._hit_endpoint("localhost", "something_else")
