cecdca38e288f15d5136e03bc8de5c4a50dbe6c1
Norton Pengra <npengra317@gmail.com>
2016-06-01 11:18:54 -0700

    updated _hit_endpoint to be more assertive (#22) _hit_endpoint will check if response.ok is True or error out

--- qwergram_bots/github/flourine_bot.py
+++ qwergram_bots/github/flourine_bot.py
@@ -3,7 +3,7 @@ import requests
 import json
 import os
 
-LOCAL_ENDPOINT = "http://127.0.0.1:8000/api/v1/"
+LOCAL_ENDPOINT = "http://127.0.0.1:8000/api/v1/stats/"
 
 
 class Flourine(object):
@@ -14,17 +14,19 @@ class Flourine(object):
         self.admin_pass = admin_pass
 
     def _hit_endpoint(self, target, verb="get", **kwargs):
-        return getattr(requests, verb)(target, **kwargs)#.json()
+        response = getattr(requests, verb)(target, **kwargs)
+        assert response.ok, response.reason
+        return response.json()
 
     def upload_data(self, name, value):
-        data = {"name": name, "value": value}
-        auth = requests.HTTPBasicAuth(self.admin, self.admin_pass)
-        response = self._hit_endpoint(self.local_endpoint, verb="post", data=data, auth=auth)
-        assert response.ok, response.json()
+        data = {"name": name, "value": json.dumps(value)}
+        response = self._hit_endpoint(self.local_endpoint, verb="post", data=data, auth=(self.admin, self.admin_pass))
+        # import pdb; pdb.set_trace()
+        assert response.ok, response.reason
 
 
 if __name__ == "__main__":
-    admin = os.environ['ADMIN_USER']
-    admin_pass = os.environ['ADMIN_PASS']
+    # admin = os.environ['ADMIN_USER']
+    # admin_pass = os.environ['ADMIN_PASS']
     FBot = Flourine(LOCAL_ENDPOINT, admin, admin_pass)
     FBot.upload_data("test", {"foo": "bar"})
