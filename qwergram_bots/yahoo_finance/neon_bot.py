fd110809dd0ed9c629344d2d3e1c685d14eb112f
Norton Pengra <npengra317@gmail.com>
2016-06-02 14:54:11 -0700

    def push_data_to_server (#91)

--- qwergram_bots/yahoo_finance/neon_bot.py
+++ qwergram_bots/yahoo_finance/neon_bot.py
@@ -1,7 +1,8 @@
 """This bot will read the Yahoo CSVs and upload new data to the server"""
 import requests
-from github.flourine_bot import LOCAL_ENDPOINT
+import os
 
+LOCAL_ENDPOINT = "http://127.0.0.1:8000/api/v1/stocks/"
 YAHOO_CSV_TARGET = "http://real-chart.finance.yahoo.com/table.csv?s={SYMB}"
 
 
@@ -27,10 +28,24 @@ class Neon(object):
             except AssertionError:
                 print("Error returning Stock:", stock)
 
+    def push_data_to_server(self, symbol, date, high, low, open, close, volume, adj_close):
+        data = {
+            "symbol": symbol,
+            "date": date,
+            "high": high,
+            "low": low,
+            "open": open,
+            "close": close,
+            "volume": volume,
+            "adj_close": adj_close,
+        }
+        self._hit_endpoint(self.local_endpoint, verb="post", data=data, auth=(self.admin, self.admin_pass))
 
 if __name__ == "__main__":
     # SP500 is ^GSPC for some reason. I'm not financially savy enough
     # to know why or if that's even true to begin with...
     stocks = "^GSPC NDX DJIA VOO VB CORP VNQ VWO SHY".split()
-    NBot = Neon(stocks, YAHOO_CSV_TARGET, LOCAL_ENDPOINT)
+    admin = os.environ['ADMIN_USER']
+    admin_pass = os.environ['ADMIN_PASS']
+    NBot = Neon(stocks, YAHOO_CSV_TARGET, LOCAL_ENDPOINT, admin, admin_pass)
     NBot.update_local_database()
