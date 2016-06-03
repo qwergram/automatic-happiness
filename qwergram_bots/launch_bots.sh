c67eedd62f49eac9aad8be09e9870f7fb9058b8a
Norton Pengra <npengra317@gmail.com>
2016-06-02 23:08:29 -0700

    Launch NeonBot part of launch_bots.sh (#91)

--- qwergram_bots/launch_bots.sh
+++ qwergram_bots/launch_bots.sh
@@ -1,6 +1,6 @@
 source "../.secrets.sh"
 echo "Booting up Hydrogen and Lithium Bot..."
-python3 -m smtp.get_articles
-
+# python3 -m smtp.get_articles
+python3 -m yahoo_finance.neon_bot
 
 echo "All bots are online."
